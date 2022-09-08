827628da241ae892f69ce358a69102f0e384c172
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-09-09 00:41:53 +0530

    Hide other job role, show only when other selected

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 //Make the first text field have the focus state by default to prompt the user.
 const nameInput = document.getElementById('name');
 nameInput.focus();
+
+
+const jobRoleOption = document.getElementById('title');
+const otherJobRoleInput = document.getElementById('other-job-role');
+//"Other job role" text field should be hidden by default 
+otherJobRoleInput.type = 'hidden';
+//Display only when users select "Other" in the drop down menu
+jobRoleOption.addEventListener("change", () => {
+    if(jobRoleOption.value === 'other'){
+        otherJobRoleInput.type = 'text';
+    } else {
+        otherJobRoleInput.type = 'hidden';
+    }
+});
